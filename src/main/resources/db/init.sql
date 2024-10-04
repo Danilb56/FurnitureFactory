d8e1aa8fc4ea9c35c0ab27cba1e56aacc4855a7b
Danilb56 <danoren1510@mail.ru>
2024-10-04 19:19:45 +0300

    foreign keys

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,17 +1,21 @@
+create table if not exists furniture_line
+(
+    id   int8 primary key,
+    name varchar(30) not null
+);
+
+
 create table if not exists furniture
 (
     id                int8 primary key,
     type              varchar(20) not null,
     article           int8        not null,
     price             int8        not null,
-    furniture_line_id int8        not null
+    furniture_line_id int8        not null,
+    foreign key (furniture_line_id) references furniture_line (id) on delete no action
 );
 
-create table if not exists furniture_line
-(
-    id   int8 primary key,
-    name varchar(30) not null
-);
+
 
 create table if not exists component
 (
@@ -24,7 +28,9 @@ create table if not exists furniture_component_link
 (
     furniture_id int8,
     component_id int8,
-    count        int
+    count        int,
+    foreign key (furniture_id) references furniture (id) on delete no action,
+    foreign key (component_id) references component (code) on delete no action
 );
 
 create table if not exists user
@@ -40,19 +46,23 @@ create table if not exists shop
     id       int8 primary key,
     address  varchar(100) not null unique,
     fax      varchar(10)  not null unique,
-    owner_id int8         not null
+    owner_id int8         not null,
+    foreign key (owner_id) references user (id) on delete no action
 );
 
 create table if not exists orders
 (
     id      int8 primary key,
     date    varchar(15) not null,
-    shop_id int8 not null
+    shop_id int8        not null,
+    foreign key (shop_id) references shop (id) on delete no action
 );
 
 create table if not exists order_furniture_link
 (
     order_id     int8 not null,
     furniture_id int8 not null,
-    count        int  not null check (count > 0)
+    count        int  not null check (count > 0),
+    foreign key (order_id) references orders (id) on delete no action,
+    foreign key (furniture_id) references furniture (id) on delete no action
 );
